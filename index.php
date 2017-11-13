8772ee37fb6e01f61899ad24257d93f0172bb97e
qudusdev <qudusdev@gmail.com>
2017-11-13 20:18:55 +0100

    Finished with the front end and the creation of the sqlite database

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
+<?php
+require_once "./app/init.php";
+require_once "../local_CDN/dBug.php";
+
+$user = $_SESSION['user_id'];
+$itemsQuery = "SELECT id,task,done FROM items WHERE username = $user";
+
+$result = $db->query($itemsQuery);
+$rows = [];
+
+while($row = $result->fetchArray(SQLITE3_ASSOC)){
+  $rows[] = $row;
+}
+// new dBug($rows);
+$items = count($rows) ? $rows : [];
+
+// echo '<pre>',print_r($items),'</pre>';
+// foreach ($items as $item ) {
+//   echo $item['task'],"<br>";
+// }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,9 +34,22 @@
 <body>
   <div class="list">
     <h1 class="header">To Do</h1>
-
+    <?php
+    if(!empty($items)){?>
     <ul class="items">
-      <li>
+    <?php
+      foreach ($items as $item ) {
+        $done = $item['done'] ? 'done':'';
+        $doneButton = $done ? '':'<a href = "#" class = "done-button">Mark as done</a>';
+        $task = $item['task'];
+        $heredoc = <<<START
+        <li><span class = "item $done">$task</span>$doneButton</li>
+START;
+        // echo("<li><span class=","</li>");
+        echo $heredoc;
+      }
+    ?>
+      <!--<li>
         <span class="item done">Code</span>
       </li>
       <li>
@@ -24,9 +58,14 @@
       <li>
         <span class="item">Go to EKSUTH</span> <a href="#" class="done-button">Mark as done</a>
 
-      </li>
+      </li>-->
 
     </ul>
+    <?php }
+      else{
+        echo "<p>You have not added any items yet</p>";
+      }
+    ?>
     <form action="add.php" method="POST" class="item-add">
       <input type="text" name="name" placeholder="Type a new item here" class="input" autocomplete="off" required>
       <input type="submit" value="Add">
